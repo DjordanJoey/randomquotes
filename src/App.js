be117c83c1c9b586461de81d7ce76693159c93ca
DjordanJoey <jordanyusup@gmail.com>
2022-09-15 16:22:00 +0700

    Hosting

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    advice: '',
+  }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+          <p className='watermark'>Dibuat dengan <span className='love'>❤</span> Oleh Joy</p>
+        </div>               
+      </div>
+    );
+  }
+}
+
+export default App;
